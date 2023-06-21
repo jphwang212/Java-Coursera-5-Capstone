ae1e865e512fc41727c0b7e9640da70709d2bc20
jphwang212 <jphwang212@gmail.com>
2023-06-20 20:21:01 -0400

    completed week1

--- src/FirstRatings.java
+++ src/FirstRatings.java
@@ -70,13 +70,88 @@ public class FirstRatings {
 //        for (Movie movie : movieData) {
 //            System.out.print(movie + " ");
 //        }
+        HashMap<String, ArrayList<String>> directorMoviesMap = new HashMap<String, ArrayList<String>>();
+        int genreCount = 0;
+        int timeCount = 0;
+        for (Movie movie : movieData) {
+            String movieId = movie.getID();
+            if (movie.getGenres().contains("Comedy")) {
+                genreCount++;
+            }
+            if (movie.getMinutes() > 150) {
+                timeCount++;
+            }
+            String[] directors = movie.getDirector().split(",");
+            for (int i = 0; i < directors.length; i++) {
+                ArrayList<String> directorMovies = new ArrayList<String>();
+                if (!directorMoviesMap.containsKey(directors[i])) {
+                    directorMovies.add(movieId);
+                    directorMoviesMap.put(directors[i], directorMovies);
+                } else {
+                    directorMovies = directorMoviesMap.get(directors[i]);
+                    if (!directorMovies.contains(movieId)) {
+                        directorMovies.add(movieId);
+                        directorMoviesMap.replace(movieId, directorMovies);
+                    }
+                }
+            }
+        }
+        int largestSize = 0;
+        ArrayList<String> directorLargest = new ArrayList<String>();
+        for (String director : directorMoviesMap.keySet()) {
+            int size = directorMoviesMap.get(director).size();
+            if (size > largestSize) {
+                largestSize = size;
+                directorLargest.clear();
+                directorLargest.add(director);
+            } else if (size == largestSize && !directorLargest.contains(director)) {
+                directorLargest.add(director);
+            }
+//            System.out.println(director + ": " + directorMoviesMap.get(director));
+        }
+        System.out.println("There are " + genreCount + " Comedy movies.");
+        System.out.println("There are " + timeCount + " movies longer than 150.");
+        System.out.println(directorLargest.toString() + " directors had movies with size " + largestSize);
     }
     public void testLoadRaters() {
         ArrayList<Rater> raterData = loadRaters("ratings.csv");
         System.out.println("There are " + raterData.size() + " raters.");
+        String raterId = "193";
+        String movieId = "1798709";
+        int maxRatings = 0;
+        HashMap<String, ArrayList<String>> movieRatingMap = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> mostRatings = new ArrayList<String>();
         for (Rater rater : raterData) {
-            System.out.print(rater.getID() + " ");
+//            System.out.print(rater.getID() + " ");
+            String currRaterId = rater.getID();
+            ArrayList<String> movies = rater.getItemsRated();
+            for (String movie : movies) {
+                if (!movieRatingMap.containsKey(movie)) {
+                    ArrayList<String> raters = new ArrayList<String>();
+                    raters.add(currRaterId);
+                    movieRatingMap.put(movie, raters);
+                } else {
+                    ArrayList<String> raters = movieRatingMap.get(movie);
+                    raters.add(currRaterId);
+                    movieRatingMap.replace(movie, raters);
+                }
+            }
+            int currentRatings = rater.getItemsRated().size();
+            if (currentRatings > maxRatings) {
+                maxRatings = currentRatings;
+                mostRatings.clear();
+                mostRatings.add(rater.getID());
+            } else if (currentRatings == maxRatings && !mostRatings.contains(rater)) {
+                mostRatings.add(rater.getID());
+            }
+            if (raterId.equals(rater.getID())) {
+                System.out.println(raterId + " has " + rater.getItemsRated().size() + " ratings.");
+            }
         }
+        System.out.println("Most ratings by " + mostRatings.toString());
+//        System.out.println("Max num of ratings = " + raterData.get().);
+        System.out.println(movieRatingMap.get(movieId).size() + " raters.");
+        System.out.println(movieRatingMap.size() + " unique movies.");
     }
 
     public static void main(String[] args) {
